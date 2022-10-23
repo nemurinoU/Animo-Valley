38adb2cb73f904c56fb02f76b1df6882c4d970e1
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2022-10-23 23:48:06 +0800

    Alpha v0.0.1
    Added the ff. classes w/ barebones implementation based on the visualization sketch:
    > MyFarm
    > Farmer
    > PlotLand
    > Inventory
    > Item

---
+++ Farmer.java
@@ -0,0 +1,17 @@
+
+public class Farmer {
+	private double xp, coins;
+	 
+	public Farmer () {
+		this.xp = 0;
+		this.coins = 100;
+	}
+	
+	public double getXP () {
+		return this.xp;
+	}
+	
+	public double getCoins () {
+		return this.coins;
+	}
+}
--- MyFarm.java
+++ MyFarm.java
@@ -1,3 +1,9 @@
-public class MyFarm {
+import java.io.*;
+import java.lang.*;
+import java.util.ArrayList;
 
+public class MyFarm {
+	public static void main (String[] args) {
+		System.out.println("test");
+	}
 }
