c888730b561c64c61798b03cdbd64c17edbc3970
richjpex <richard_pecson@dlsu.edu.ph>
2022-11-01 18:41:38 +0800

    Co-authored-by: nemurinoU <nemurinoU@users.noreply.github.com>

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,29 +1,60 @@
-
+import java.util.ArrayList;
 public class Farmer {
 	private String name;
-	private double xp, coins;
-	private int lvl;
-	 
-	public Farmer (String name) {
-		this.xp = 0;
-		this.coins = 100;
-		this.lvl = 0;
+	private double xp = 0, coins = 100;
+	private int lvl = 0;
+	private ArrayList<Item> items;
+
+	public Farmer(String name, double xp, double coins, int lvl, ArrayList<Item> items) {
+		this.name = name;
+		this.xp = xp;
+		this.coins = coins;
+		this.lvl = lvl;
+		this.items = new ArrayList<Item>();
+	}
+
+	public Farmer(String name){
 		this.name = name;
 	}
-	
-	public double getXP () {
-		return this.xp;
-	}
-	
-	public double getCoins () {
-		return this.coins;
-	}
-	
-	public int getLvl () {
-		return this.lvl;
-	}
-	
-	public String getName () {
+
+	public String getName() {
 		return this.name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getXp() {
+		return this.xp;
+	}
+
+	public void setXp(double xp) {
+		this.xp = xp;
+	}
+
+	public double getCoins() {
+		return this.coins;
+	}
+
+	public void setCoins(double coins) {
+		this.coins = coins;
+	}
+
+	public int getLvl() {
+		return this.lvl;
+	}
+
+	public void updateLvl() {
+		this.lvl = (int)this.xp / 100;
+	}
+
+	public ArrayList<Item> getItems() {
+		return this.items;
+	}
+
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
+	
 }
